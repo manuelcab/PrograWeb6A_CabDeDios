8beeb15a9c3dbd16f0a2fd11821a1099a573b609
manuelcab <manuel110010101011@hotmail.com>
2024-02-21 18:29:46 -0600

    index 1.1

--- usuarios/index.php
+++ usuarios/index.php
@@ -4,21 +4,37 @@ include 'Usuario.php';
 
 $usuarioDAO = new UsuarioDAO();
 
-$usuario = new Usuario();
-$usuario->setNombres("Bugs");
-$usuario->setApellidos("Bunny");
-$usuario->setCorreo("bugsbunny@wb.com");
+$bugs = new Usuario();
+$bugs->setNombres("Bugs");
+$bugs->setApellidos("Bunny");
+$bugs->setCorreo("bugsbunny@wb.com");
+$bugs->guardar();
 
-$usuario2 = new Usuario();
-$usuario2->setNombres("Lola");
-$usuario2->setApellidos("Bunny");
-$usuario2->setCorreo("lolabunny@wb.com");
+$lola = new Usuario();
+$lola->setNombres("Lola");
+$lola->setApellidos("Bunny");
+$lola->setCorreo("lolabunny@wb.com");
+$lola->guardar();
 
-$usuarioDAO->insertar($usuario);
-$usuarioDAO->insertar($usuario2);
+$lucas = new Usuario();
+$lucas->setNombres("Lucas");
+$lucas->setApellidos("Duck");
+$lucas->setCorreo("patolucas@wb.com");
+$lucas->guardar();
+
+$porky = new Usuario();
+$porky->setNombres("Porky");
+$porky->setApellidos("Pig");
+$porky->setCorreo("porkypigs@wb.com");
+$porky->guardar();
 
 $usuarios = $usuarioDAO->buscarTodos();
 
 foreach($usuarios as $usuario){
     echo $usuario->getNombres(). ' ' . $usuario->getApellidos() . ' ' . $usuario->getCorreo() . '<br>';
 }
+
+echo '<br>';
+
+$porky->setCorreo("porkypig@wb.com");
+$porky->actualizar();
