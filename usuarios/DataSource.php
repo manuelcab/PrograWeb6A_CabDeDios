b93e5bb8422b1d9cefcea58f594ef1224e24fdcb
manuelcab <manuel110010101011@hotmail.com>
2024-02-21 19:00:24 -0600

    añadi el .env al gitignore

--- usuarios/DataSource.php
+++ usuarios/DataSource.php
@@ -7,33 +7,38 @@ class DataSource {
     private $conexion;
 
     public function __construct() {
-        try{
-            $this->cadenaParaConexion = "mysql:host=localhost;dbname=prueba";
-            $this->conexion = new PDO($this->cadenaParaConexion, "root", "root");
+        try {
+          $host = EnvLoader::get('DB_HOST');
+          $dbname = EnvLoader::get('DB_DATABASE');
+          $user = EnvLoader::get('DB_USERNAME');
+          $password = EnvLoader::get('DB_PASSWORD');
+    
+          $this->cadenaParaConexion = "mysql:host=$host;dbname=$dbname";
+          $this->conexion = new PDO($this->cadenaParaConexion, $user, $password);
         } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
+          echo "Error: " . $e->getMessage();
         }
-    }
+      }
 
-    public function ejecutarConsulta($sql = "", $values = []){
-        if($sql !=""){
-            $consulta = $this->conexion->prepare($sql);
-            $consulta->execute($values);
-            return $consulta->fetchAll(PDO::FETCH_ASSOC);
-        }else{
-            return 0;
+      public function ejecutarConsulta($sql = "", $values = []) {
+        if ($sql != "") {
+          $consulta = $this->conexion->prepare($sql);
+          $consulta->execute($values);
+          return $consulta->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+          return 0;
         }
-    }
+      }
 
-    public function ejecutarActualizacion($sql = "", $values = []){
-        if($sql !=""){
-            $consulta = $this->conexion->prepare($sql);
-            $consulta->execute($values);
-            return $consulta->rowCount();
-        }else{
-            return 0;
+      public function ejecutarActualizacion ($sql = "", $values = []) {
+        if ($sql != "") {
+          $consulta = $this->conexion->prepare($sql);
+          $consulta->execute($values);
+          return $consulta->rowCount();
+        } else {
+          return 0;
         }
-    }
+      }
 
     public function obtenerUltimoIdInsertado() {
         return $this->conexion->lastInsertId();
