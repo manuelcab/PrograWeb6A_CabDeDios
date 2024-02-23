444b689578324efc09947c7f2498e6506bb6b1ae
manuelcab <manuel110010101011@hotmail.com>
2024-02-23 11:33:27 -0600

    ADD DataSource.php

---
+++ catalogo/php/EnvLoader.php
@@ -0,0 +1,29 @@
+<?php
+
+class EnvLoader {
+  public static function get($key)
+  {
+    $envFile = __DIR__ . '/.env';
+    if (!file_exists($envFile)) {
+      throw new Exception('.env file not found.');
+    }
+
+    $envContent = file_get_contents($envFile);
+    $lines = explode("\n", $envContent);
+    $envVariables = [];
+
+    foreach ($lines as $line) {
+      $line = trim($line);
+      if (!empty($line) && strpos($line, '=') !== false) {
+        list($envKey, $envValue) = explode('=', $line, 2);
+        $envVariables[$envKey] = $envValue;
+      }
+    }
+
+    if (isset($envVariables[$key])) {
+      return $envVariables[$key];
+    } else {
+      throw new Exception('Key not found in .env file.');
+    }
+  }
+}
